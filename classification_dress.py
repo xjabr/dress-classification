f8fd241e6d007b0481d73a2cd1a3ec502a511ab9
Gabriele Lanzafame <gabriele.lanzafame03@gmail.com>
2021-03-30 15:59:50 +0200

    init project

---
+++ classification_dress.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import random
+
+def create_model():
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+def fit_model(model, images, labels, epochs):
+    model.fit(images, labels, epochs=epochs)
+
+def predict(model, images, labels, class_names):
+    n_tests = len(images)
+    random_index = random.randint(0, n_tests-1)
+
+    print('Random Index ', random_index)
+
+    img = images[random_index]
+    img = (np.expand_dims(img, 0))
+
+    prediction = model.predict(img)
+    true_label = labels[random_index]
+    predicted_label = np.argmax(prediction)
+
+    print('True: {}\nPredicted: {}'.format(class_names[true_label], class_names[predicted_label]))
+
+    # show image
+    plt.figure(figsize=(10, 10))
+    plt.imshow(images[random_index])
+
+    plt.show()
---
+++ save_model.py
@@ -0,0 +1,21 @@
+from classification_dress import create_model, fit_model, predict
+
+import tensorflow as tf
+from tensorflow import keras
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = [ 'T-shirt / top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot' ]
+
+# normalize values
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+model = create_model()
+fit_model(model, train_images, train_labels, 5)
+
+# save models
+model.summary()
+
+model.save('./model')
